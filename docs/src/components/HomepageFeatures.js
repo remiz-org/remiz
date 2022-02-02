4330147b7cbcd4651dbccc5a398d216bb1a80ebe
Hugues Gaillard <hugues.gaillard@me.com>
2022-02-02 12:46:55 +0100

    edit docs

--- docs/src/components/HomepageFeatures.js
+++ docs/src/components/HomepageFeatures.js
@@ -8,28 +8,28 @@ const FeatureList = [
     Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Remiz was designed from the ground up to be easily installed and
+        used to build and deploy your packages quickly.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Extensible',
     Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Write your configuration in <code>TOML</code> and use any
+        programming language to build/deploy components of your projet.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
+    title: 'Powered by Rust',
+    Svg: require('../../static/img/rustacean-flat-happy.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Remiz is written in Rust. Remiz aims to be reliable, secure, fast.
+        The best of all ? The project is open source (MIT licensed).
       </>
     ),
   },

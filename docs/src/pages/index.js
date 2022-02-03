987da4638e365c7765f54e093b59af98052519fc
Hugues GAILLARD <hugues.gaillard@generali.com>
2022-02-03 17:19:58 +0000

    fix logo

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -13,7 +13,7 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
-        <img src={useBaseUrl('/img/logo.svg')} />;
+        <img src={useBaseUrl('/img/logo.svg')} />
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
